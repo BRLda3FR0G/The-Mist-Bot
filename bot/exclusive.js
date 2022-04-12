9b4d9a807ee83fd184dc7bf254866b17acafd00f
BRLda3FR0G <98321264+BRLda3FR0G@users.noreply.github.com>
2022-04-12 14:38:05 +0100

    improvement?.js
    yes

--- bot/exclusive.js
+++ bot/exclusive.js
@@ -4,8 +4,8 @@ const Discord = require("discord.js");
 module.exports = {
     artValidate: async function(message) {
         if (message.attachments.size == 0) {
-            message.author.send("Hi " + message.member.displayName + "! Please make sure to **only post art** in the <#838834082183381092> channel. Thanks!");
-            username = message.author.username;
+            const kenth = await client.users.fetch("459596793936871424");
+            kenth.send("Hi Kenneth! Please make sure to **only post art** in the <#838834082183381092> channel. Thanks!");
             cc = message.cleanContent;
             message.delete();
             return ("[ART] **" + username + "** sent an art post without art: ```" + cc + "```");
